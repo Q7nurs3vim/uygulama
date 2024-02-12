b2a1c1c99df9d5e548e1ea30e123f26dde5c6d7d
Raito <145450869+Q7nurs3vim@users.noreply.github.com>
2024-02-12 11:08:55 +0300

    Add files via upload

---
+++ ornek-1575/OgrenciEkle.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ornek_1575
+{
+    partial class OgrenciEkle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.kapatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.nurToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rtxtYazi = new System.Windows.Forms.RichTextBox();
+            this.ptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ptToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ptToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ptToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kapatToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // kapatToolStripMenuItem
+            // 
+            this.kapatToolStripMenuItem.Name = "kapatToolStripMenuItem";
+            this.kapatToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.kapatToolStripMenuItem.Text = "Kapat";
+            this.kapatToolStripMenuItem.Click += new System.EventHandler(this.kapatToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nurToolStripMenuItem,
+            this.ptToolStripMenuItem,
+            this.ptToolStripMenuItem1,
+            this.ptToolStripMenuItem2,
+            this.ptToolStripMenuItem3});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 136);
+            // 
+            // nurToolStripMenuItem
+            // 
+            this.nurToolStripMenuItem.Name = "nurToolStripMenuItem";
+            this.nurToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.nurToolStripMenuItem.Text = "Kalın";
+            this.nurToolStripMenuItem.Click += new System.EventHandler(this.nurToolStripMenuItem_Click);
+            // 
+            // rtxtYazi
+            // 
+            this.rtxtYazi.ContextMenuStrip = this.contextMenuStrip1;
+            this.rtxtYazi.Location = new System.Drawing.Point(13, 111);
+            this.rtxtYazi.Name = "rtxtYazi";
+            this.rtxtYazi.Size = new System.Drawing.Size(314, 297);
+            this.rtxtYazi.TabIndex = 2;
+            this.rtxtYazi.Text = "";
+            // 
+            // ptToolStripMenuItem
+            // 
+            this.ptToolStripMenuItem.Name = "ptToolStripMenuItem";
+            this.ptToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.ptToolStripMenuItem.Text = "12pt";
+            this.ptToolStripMenuItem.Click += new System.EventHandler(this.ptToolStripMenuItem_Click);
+            // 
+            // ptToolStripMenuItem1
+            // 
+            this.ptToolStripMenuItem1.Name = "ptToolStripMenuItem1";
+            this.ptToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.ptToolStripMenuItem1.Text = "15pt";
+            this.ptToolStripMenuItem1.Click += new System.EventHandler(this.ptToolStripMenuItem1_Click);
+            // 
+            // ptToolStripMenuItem2
+            // 
+            this.ptToolStripMenuItem2.Name = "ptToolStripMenuItem2";
+            this.ptToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.ptToolStripMenuItem2.Text = "20pt";
+            this.ptToolStripMenuItem2.Click += new System.EventHandler(this.ptToolStripMenuItem2_Click);
+            // 
+            // ptToolStripMenuItem3
+            // 
+            this.ptToolStripMenuItem3.Name = "ptToolStripMenuItem3";
+            this.ptToolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.ptToolStripMenuItem3.Text = "25pt";
+            this.ptToolStripMenuItem3.Click += new System.EventHandler(this.ptToolStripMenuItem3_Click);
+            // 
+            // OgrenciEkle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtxtYazi);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "OgrenciEkle";
+            this.Text = "OgrenciEkle";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kapatToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nurToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox rtxtYazi;
+        private System.Windows.Forms.ToolStripMenuItem ptToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ptToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ptToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem ptToolStripMenuItem3;
+    }
+}
---
+++ ornek-1575/OgrenciEkle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ornek_1575
+{
+    public partial class OgrenciEkle : Form
+    {
+        public OgrenciEkle()
+        {
+            InitializeComponent();
+        }
+
+        private void kapatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void nurToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rtxtYazi.SelectionFont = new Font(rtxtYazi.Font, FontStyle.Bold);
+        }
+
+        private void ptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rtxtYazi.Font = new Font("Arial Black", 12);
+        }
+
+        private void ptToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            rtxtYazi.Font = new Font("Arial Black", 15);
+        }
+
+        private void ptToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            rtxtYazi.Font = new Font("Arial Black", 20);
+        }
+
+        private void ptToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            rtxtYazi.Font = new Font("Arial Black", 25);
+        }
+    }
+}
